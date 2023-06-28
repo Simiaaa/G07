03b62b5bf904d413d7e2ac39dd570170b49ce256
任欣雨 <137738501+Xindyuuuu@users.noreply.github.com>
2023-06-28 09:36:44 +0800

    Update folder.sh
    后端

--- backend/folder.sh
+++ backend/folder.sh
@@ -1,8 +1,29 @@
- #获取文件夹
-ls -F | grep "/$"> 1.json
+#获取根目录
+ls / > disk.txt
+#输出格式
+echo "{" >folders.json
+echo "    \"folders\": [ " >> folders.json
+#遍历每一个根目录下的文件
+a=0
+count=$(cat disk.txt | wc -l)
+for i in `cat disk.txt`
+do      
+echo "      {">> folders.json
+#输出该根目录id
+echo "        \"id\":"$((++a))",">> folders.json
+#输出该根目录name
+echo "        \"name\": \""$i"\"">> folders.json
+#输出该文件下根目录
+echo "        \"folders\": [ " >> folders.json
+echo "          {">> folders.json
+find /$i -type d |awk -F "/" '{print $3 }'|sort -n |uniq > folders.txt
+cat  folders.txt|tr -s '\n'
 awk   -F "/"  '{print $1}' 1.json > 2.json
-echo "{" >1.json
-echo "  \"folders\": [ " >> 1.json
 awk '{printf "    \"" $1 "\"" }NR<'`cat 2.json | wc -l`' {printf ",\n"}END{printf "\n"}' 2.json >>1.json
-echo "  ]" >>1.json
-echo "}" >>1.json
+if [ $a -eq $count ]
+then echo "    }">> folders.json
+else echo "    },">> folders.json
+fi
+done
+echo "    ]" >> folders.json
+echo "  }" >> folders.json
