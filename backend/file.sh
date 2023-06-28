4337cd18fb00a7261a999c535f9e7260c228e6e8
任欣雨 <137738501+Xindyuuuu@users.noreply.github.com>
2023-06-29 03:41:45 +0800

    优化格式

--- backend/file.sh
+++ backend/file.sh
@@ -1,10 +1,24 @@
-#获取文件夹下word和pdf文件
-find folder1 -name "*.doc" > 1.json
-find folder1 -name "*.docx" >> 1.json
-find folder1 -name "*.pdf" >> 1.json
-awk -F "/" '{print $2}' 1.json > 2.json
-echo "{" >1.json
-echo "  \"files\": [ " >> 1.json
-awk '{printf "    \"" $1 "\"" }NR<'`cat 2.json | wc -l`' {printf ",\n"}END{printf "\n"}' 2.json >>1.json
-echo "  ]" >> 1.json
-echo "}" >> 1.json
+#!/bin/bash
+
+# 指定的文件夹路径
+directory="/root/folder1"  
+
+# 获取文件夹下的Word和PDF文件，并保存到file.txt
+ls -RF "$directory" | grep -v "/" | sed -e '/^$/d' > file.txt
+
+# 生成file.json文件
+echo "{" > file.json
+echo "  \"files\": [" >> file.json
+
+awk -F '.' '{
+    print "    {"
+    print "      \"id\": " NR ","
+    print "      \"name\": \"" $1 "." $2 "\","
+    print "      \"type\": \"" $2 "\""
+    print "    }"
+}
+NR < '`cat file.txt | wc -l`' { printf ",\n" }
+END { printf "\n" }' file.txt >> file.json
+
+echo "  ]" >> file.json
+echo "}" >> file.json
